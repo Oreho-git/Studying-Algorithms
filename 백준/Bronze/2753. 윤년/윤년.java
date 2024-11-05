b90943a02d5f9666126a5c945afd37b3360ecede
Oreho-git <jshooho@gmail.com>
2024-11-05 20:32:07 +0900

    [Bronze V] Title: 윤년, Time: 172 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int year = scanner.nextInt();
+        if (year < 1 || year > 4000) {
+            return; 
+        }
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0); 
+        }
+        scanner.close(); 
+    }
+}
